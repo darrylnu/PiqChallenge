3c6b86173423e02e2d31cb88a23df80bcc88212c
Darryl Nunn <darrylnu@gmail.com>
2016-02-15 05:38:44 -0500

    added feat: if user is already signed in, the crew will automatically go to next view

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -16,13 +16,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UIAdapt
     
     
     @IBOutlet var userNameField: UITextField!
-
     @IBOutlet var passwordField: UITextField!
-    
     @IBOutlet var registeredLabel: UILabel!
     @IBOutlet var formLabel: UILabel!
     @IBOutlet var loginLabel: UIButton!
-    
     @IBOutlet var signUpLabel: UIButton!
     
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
@@ -55,29 +52,59 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UIAdapt
             activityIndicator.startAnimating()
             UIApplication.sharedApplication().beginIgnoringInteractionEvents()
             
-            //sign up user
-            var user = PFUser()
-            user.username = userNameField.text
-            user.password = passwordField.text
             var errorMessage = "Please try again later."
-            user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+            
+            
+            if signupActive {
+                //sign up user
+                let user = PFUser()
+                user.username = userNameField.text
+                user.password = passwordField.text
+                user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    if error == nil {
+                        //signup successful
+                        self.performSegueWithIdentifier("login", sender: self)
+
+                       
+                    } else {
+                        if let errorString = error?.userInfo["error"] as? String {
+                            
+                            errorMessage = errorString
+                            self.alerter("Failed Sign up", message: errorMessage, actionTitle: "Try Again")
+                            
+                            
+                        }
+                    }
+                    
+                })
                 
-                self.activityIndicator.stopAnimating()
-                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+            } else {
                 
-                if error == nil {
-                    //signup successful
-                } else {
-                    if let errorString = error?.userInfo["error"] as? String {
+                PFUser.logInWithUsernameInBackground(userNameField.text!, password: passwordField.text!, block: { (user, error) -> Void in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    if user != nil {
+                        //login successful
+                        self.performSegueWithIdentifier("login", sender: self)
                         
-                        errorMessage = errorString
-                        self.alerter("Failed Sign up", message: errorMessage, actionTitle: "Try Again")
-                        
+                    } else {
+                    
+                        if let errorString = error?.userInfo["error"] as? String {
+                            //alert user of error
+                            errorMessage = errorString
+                            self.alerter("Failed Log In", message: errorMessage, actionTitle: "Try Again")
+                        }
                         
                     }
-                }
-                
-            })
+                    
+                })
+            }
             
             
         }
@@ -114,6 +141,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UIAdapt
         // Do any additional setup after loading the view, typically from a nib.
         
        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        if PFUser.currentUser() != nil {
+            self.performSegueWithIdentifier("login", sender: self)
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
